5108e43264e59d8ba80282ac21e2f27285ba5f52
nayem-ahmedz <nayemahmed2n@gmail.com>
2025-09-04 22:13:05 +0600

    show lessons section with placeholder lesson is done, fetched avaialvle lessons name using API

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('connected');
+// show lessons
+function fetchLessons(){
+    const url = 'https://openapi.programming-hero.com/api/levels/all';
+    fetch(url)
+    .then(res => res.json())
+    .then(json => showLessons(json.data));
+}
+const lessonsCont = document.getElementById('lessons-container');
+function showLessons(lessons){
+    lessons.forEach(lesson => {
+        const button = document.createElement('button');
+        button.innerHTML = `<i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}`;
+        button.classList.add('btn', 'btn-outline', 'btn-primary');
+        lessonsCont.appendChild(button);
+    });
+}
+fetchLessons();
